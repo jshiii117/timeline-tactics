b05d8203888e666a43b9faea6935212cc27630f0
James Shi <jamesshi117@gmail.com>
2022-08-07 14:53:32 -0700

    Set Canvas as Mirror Player Prefab in unitSelect scene

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class CanvasScript : MonoBehaviour
+public class CanvasScript : NetworkBehaviour
 {
 
     public GameObject monk;
@@ -11,8 +11,23 @@ public class CanvasScript : MonoBehaviour
     void Start()
     {
 
-        this.GetComponent<Canvas>().worldCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        this.GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         
+    }
+
+    private void Update()
+    {
+        //this.GetComponent<Canvas>().worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        //this.GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        //Debug.Log("Is this working lol");
+
+    }
+
+    void OnStartServer()
+    {
+        base.OnStartServer();
+        this.GetComponent<Canvas>().worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
     }
 
     public void OnButtonPress()
