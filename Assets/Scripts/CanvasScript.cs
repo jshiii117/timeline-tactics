e5054f92be1451ca3343a68a518a752ea8a8de02
James Shi <jamesshi117@gmail.com>
2022-08-08 12:41:47 -0700

    Fix Canvas and child GameObjects as player prefabs

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -1,26 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 
 public class CanvasScript : NetworkBehaviour
 {
 
     public GameObject monk;
+    [SerializeField] Selection selectionManager;
 
     void Start()
     {
 
-        this.GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-        
+        GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        selectionManager = GameObject.Find("ScriptManager").GetComponent<Selection>();
+        selectionManager.infoText = GameObject.Find("infoText").GetComponent<Text>();
+        selectionManager.titleText = GameObject.Find("titleText").GetComponent<Text>();
+        selectionManager.classText = GameObject.Find("classText").GetComponent<Text>();
+        selectionManager.hpText = GameObject.Find("hpText").GetComponent<Text>();
+        selectionManager.attackText = GameObject.Find("attackText").GetComponent<Text>();
+        selectionManager.speedText = GameObject.Find("speedText").GetComponent<Text>();
+        selectionManager.passiveText = GameObject.Find("passiveText").GetComponent<Text>();
+        selectionManager.specialText = GameObject.Find("specialText").GetComponent<Text>();
+
+        //Selection Button
+        Button selectionButton = GameObject.Find("selectionButton").GetComponent<Button>();
+        selectionManager.selectionButton = selectionButton;
+        selectionButton.onClick.AddListener(delegate { selectionManager.ConfirmSelection(); });
+
+        //Left and Right Button
+        GameObject.Find("leftButton").GetComponent<Button>().onClick.AddListener(delegate { selectionManager.LastPage(); });
+        GameObject.Find("rightButton").GetComponent<Button>().onClick.AddListener(delegate { selectionManager.NextPage(); });
+
+
     }
 
     private void Update()
     {
-        //this.GetComponent<Canvas>().worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        //this.GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-        //Debug.Log("Is this working lol");
-
+        foreach (GameObject playerPrefab in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (playerPrefab.GetComponent<NetworkIdentity>().isLocalPlayer != true)
+            {
+                Debug.Log("Not local player");
+                playerPrefab.SetActive(false);
+                
+            }
+        }
     }
 
     void OnStartServer()
